b17cc98fe4be4f13aefcfbf8208093ef2d18d588
NIMITHASHREE <nimithashreea@gmail.com>
2026-09-23 11:00:45 +0530

    Add Calculator class with basic arithmetic operations

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addition = " + add(a, b));
+        System.out.println("Subtraction = " + subtract(a, b));
+        System.out.println("Multiplication = " + multiply(a, b));
+        System.out.println("Division = " + divide(a, b));
+    }
+}
